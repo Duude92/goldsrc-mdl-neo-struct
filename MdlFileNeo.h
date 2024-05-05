55a8bfc6d69efaa9fe3f1fd554043015d7872f5b
Duude92 <switch4acc@gmail.com>
2024-05-05 20:58:01 +0300

    Couple updates to be sync with offsets

--- MdlFileNeo.h
+++ MdlFileNeo.h
@@ -37,223 +37,265 @@
 
 #pragma pack(1)
 
-
 struct Vector3 {
-	float x;
-	float y;
-	float z;
+  float x;
+  float y;
+  float z;
 };
 
 struct Bone {
-	char BoneName[32];
-	int parent;
-	int flags;
-	int bonecontroller[6];
-	float value[6];
-	float scale[6];
+  char BoneName[32];
+  int parent;
+  int flags;
+  int bonecontroller[6];
+  float value[6];
+  float scale[6];
 };
 
 struct BoneController {
-	int bone;
-	int type;
-	float start;
-	float end;
-	int rest;
-	int index;
+  int bone;
+  int type;
+  float start;
+  float end;
+  int rest;
+  int index;
 };
 
 struct HitBox {
-	int bone;
-	int group;
-	Vector3 bbmin;
-	Vector3 bbmax;
+  int bone;
+  int group;
+  Vector3 bbmin;
+  Vector3 bbmax;
 };
 
 struct SequenceGroup {
-	char label[32];
-	char name[64];
-	int unused1;
-	int unused2;
+  char label[32];
+  char name[64];
+  int unused1;
+  int unused2;
 };
 
 struct Sequence {
-	char label[32];
-	float fps;
-	int flags;
-	int activity;
-	int actweight;
-	int numevents;
-	int eventindex;
-	int numframes;
-	int numpivots;
-	int pivotindex;
-	int motiontype;
-	int motionbone;
-	Vector3 linearmovement;
-	int automoveposindex;
-	int automoveangleindex;
-	Vector3 bbmin;
-	Vector3 bbmax;
-	int numblends;
-	int animindex;
-	int blendtype[2];
-	float blendstart[2];
-	float blendend[2];
-	int blendparent;
-	int seqgroup;
-	int entrynode;
-	int exitnode;
-	int nodeflags;
-	int nextseq;
+  char label[32];
+  float fps;
+  int flags;
+  int activity;
+  int actweight;
+  int numevents;
+  int eventindex;
+  int numframes;
+  int numpivots;
+  int pivotindex;
+  int motiontype;
+  int motionbone;
+  Vector3 linearmovement;
+  int automoveposindex;
+  int automoveangleindex;
+  Vector3 bbmin;
+  Vector3 bbmax;
+  int numblends;
+  int animindex;
+  int blendtype[2];
+  float blendstart[2];
+  float blendend[2];
+  int blendparent;
+  int seqgroup;
+  int entrynode;
+  int exitnode;
+  int nodeflags;
+  int nextseq;
 };
 
-
-struct TextureData{
-	unsigned char Data[width * height];
-	unsigned char Palette[256 * 3];
-
+struct TextureData {
+  unsigned char Data[width * height];
+  unsigned char Palette[256 * 3];
 };
-struct TextureHeader{
-	char name[64];
-	int flags;
-	int width;
-	int height;
-	int index;
+struct TextureHeader {
+  char name[64];
+  int flags;
+  int width;
+  int height;
+  int index;
 };
 public struct Texture {
-	TextureHeader Header;
-	TextureData Data;
+  TextureHeader Header;
+  TextureData Data;
 };
 struct Bodypart {
-	char name[64];
-	int nummodels;
-	int base;
-	int modelindex;
+  char name[64];
+  int nummodels;
+  int base;
+  int modelindex;
 };
 
 struct Mesh {
-	int numtris;
-	int triindex;
-	int skinref;
-	int numnorms;
-	int normindex;
+  int numtris;
+  int triindex;
+  int skinref;
+  int numnorms;
+  int normindex;
 };
 
 struct Model {
-	char name[64];
-	int type;
-	float boundingradius;
-	int nummesh;
-	int meshindex;
-	int numverts;
-	int vertinfoindex;
-	int vertindex;
-	int numnorms;
-	int norminfoindex;
-	int normindex;
-	int numgroups;
-	int groupindex;
+  char name[64];
+  int type;
+  float boundingradius;
+  int nummesh;
+  int meshindex;
+  int numverts;
+  int vertinfoindex;
+  int vertindex;
+  int numnorms;
+  int norminfoindex;
+  int normindex;
+  int numgroups;
+  int groupindex;
 };
 
 struct UV {
-	short horizontal;
-	short vertical;
+  short horizontal;
+  short vertical;
 };
 
 struct TriVert {
-	short vertindex;
-	short normindex;
-	UV UV;
+  short vertindex;
+  short normindex;
+  UV UV;
 };
 
 struct TriSequence {
-	short type;
-	TriVert TriVerts[((type >> (2 * 8 - 1)) + type) ^ (type >> (2 * 8 - 1))];
+  short type;
+  TriVert TriVerts[((type >> (2 * 8 - 1)) + type) ^ (type >> (2 * 8 - 1))];
+  if (type == 0)
+    $break_array(true);
 };
 
 struct Header {
-	char IDS[4];
-	int Version;
-	char Filename[64];
-	int Length;
-	Vector3 EyePosition;
-	Vector3 HullMin;
-	Vector3 HullMax;
-	Vector3 BBMin;
-	Vector3 BBmax;
-	int Flags;
-	int BoneCount;
-	int BoneIndex;
-	int numbonecontrollers;
-	int bonecontrollerindex;
-	int numhitboxes;
-	int hitboxindex;
-	int numseq;
-	int seqindex;
-	int numseqgroups;
-	int seqgroupindex;
-	int numtextures;
-	int textureindex;
-	int texturedataindex;
-	int numskinref;
-	int numskinfamilies;
-	int skinindex;
-	int numbodyparts;
-	int bodypartindex;
-	int numattachments;
-	int attachmentindex;
-	int soundtable;
-	int soundindex;
-	int soundgroups;
-	int soundgroupindex;
-	int numtransitions;
-	int transitionindex;
+  char IDS[4];
+  int Version;
+  char Filename[64];
+  int Length;
+  Vector3 EyePosition;
+  Vector3 HullMin;
+  Vector3 HullMax;
+  Vector3 BBMin;
+  Vector3 BBmax;
+  int Flags;
+  int BoneCount;
+  int BoneIndex;
+  int numbonecontrollers;
+  int bonecontrollerindex;
+  int numhitboxes;
+  int hitboxindex;
+  int numseq;
+  int seqindex;
+  int numseqgroups;
+  int seqgroupindex;
+  int numtextures;
+  int textureindex;
+  int texturedataindex;
+  int numskinref;
+  int numskinfamilies;
+  int skinindex;
+  int numbodyparts;
+  int bodypartindex;
+  int numattachments;
+  int attachmentindex;
+  int soundtable;
+  int soundindex;
+  int soundgroups;
+  int soundgroupindex;
+  int numtransitions;
+  int transitionindex;
 };
-struct TriSequences{
-	while(1)
-	{
-		TriSequence CurrentTriSequence;
 
-		if(CurrentTriSequence.type== 0) break;
-	}
+struct Attachment {
+  char name[32];
+  int type;
+  int bone;
+  Vector3 org;
+  Vector3 vectors[3];
 };
-public struct Textures{
-	TextureHeader TextureHeaders[1];
-	for(var i = 0; i<1; i=i+1)
-	{
-		struct TextureDataStruct{
-			unsigned char Data[TextureHeaders[i].width * TextureHeaders[i].height];
-			unsigned char Palette[256 * 3];
-		};
-		TextureDataStruct textureData;
-	}
+public struct Textures {
+  TextureHeader TextureHeaders[1];
+  for (var i = 0; i < 1; i = i + 1) {
+    struct TextureDataStruct {
+      unsigned char Data[TextureHeaders[i].width * TextureHeaders[i].height];
+      unsigned char Palette[256 * 3];
+    };
+    TextureDataStruct textureData;
+  }
+};
+struct Anim {
+  unsigned char data[24];
 };
 public struct MdlFile {
-	Header Header;
-	Bone Bones[Header.BoneCount];
-	BoneController BoneControllers[Header.numbonecontrollers];
-	HitBox HitBoxes[Header.numhitboxes];
-	unsigned char Anim[24];
-	Sequence sequences[Header.numseq];
-	SequenceGroup SequenceGroups[Header.numseqgroups];
-	Bodypart bodyparts[Header.numbodyparts];
-	Model Model;
-	unsigned char VertexBones[Model.numverts];
-	unsigned char Normalbones[Model.numnorms];
-	Vector3 Vertices[Model.numverts];
-	Vector3 Normals[Model.numnorms];
-	Mesh Meshs[Model.nummesh];
-	// TriSequences SequencesTri;
-	unsigned char TriSeq[Header.textureindex - Meshs[0].triindex];
+  Header Header;
+  Bone Bones[Header.BoneCount];
+  BoneController BoneControllers[Header.numbonecontrollers];
+  Attachment attachments[Header.numattachments];
+  HitBox HitBoxes[Header.numhitboxes];
+  var offset_backup = current_offset;
 
+  $shift_by(Header.seqindex - offset_backup);
+  Sequence sequences[Header.numseq];
+  var offset_sequences = current_offset;
 
-	TextureHeader TextureHeaders[Header.numtextures];
-	for (var i = 0; i < Header.numtextures; i = i + 1)
-	{
-		struct TextureDataStruct{
-			unsigned char Data[TextureHeaders[i].width * TextureHeaders[i].height];
-			unsigned char Palette[256 * 3];
-		};
-		TextureDataStruct textureData;
-	}
+  $shift_by(offset_backup - current_offset);
+  struct Animations {
+    for (var i = 0; i < Header.numseq; i = i + 1) {
+      Anim animation[Header.numseq];
+    }
+  };
+  Animations animations;
+  $shift_by(offset_sequences - current_offset);
+  SequenceGroup SequenceGroups[Header.numseqgroups];
+  Bodypart bodyparts[Header.numbodyparts];
+  var modelCount = 0;
+  for (var i = 0; i < Header.numbodyparts; i = i + 1) {
+    Model Models[bodyparts[i].nummodels];
+    modelCount = modelCount + 1;
+  }
+  struct TriSequences {
+    TriSequence Sequence[*];
+  };
+  for (var i = 0; i < Header.numbodyparts; i = i + 1) {
+
+    for (var j = 0; j < bodyparts[i].nummodels; j = j + 1) {
+      struct ModelData {
+        $shift_by(bodyparts[i].Models[j].vertinfoindex - current_offset);
+        unsigned char VertexBones[bodyparts[i].Models[j].numverts];
+        $shift_by(bodyparts[i].Models[j].norminfoindex - current_offset);
+        unsigned char Normalbones[bodyparts[i].Models[j].numnorms];
+        $shift_by(bodyparts[i].Models[j].vertindex - current_offset);
+        Vector3 Vertices[bodyparts[i].Models[j].numverts];
+        $shift_by(bodyparts[i].Models[j].normindex - current_offset);
+        Vector3 Normals[bodyparts[i].Models[j].numnorms];
+        $shift_by(bodyparts[i].Models[j].meshindex - current_offset);
+        Mesh Meshs[bodyparts[i].Models[j].nummesh];
+        for (var l = 0; l < bodyparts[i].Models[j].nummesh; l = l + 1) {
+          $shift_by(Meshs[l].triindex - current_offset);
+
+          TriSequences SequencesTri;
+        }
+      };
+      ModelData modelData;
+    }
+  }
+
+  // Mesh Meshs[Models[0].nummesh];
+
+  // unsigned char TriSeq[Header.textureindex - Meshs[0].triindex];
+        $shift_by(Header.textureindex - current_offset);
+  TextureHeader TextureHeaders[Header.numtextures];
+
+  unsigned short Skins[Header.numskinref * Header.numskinfamilies];
+
+  for (var i = 0; i < Header.numtextures; i = i + 1) {
+    struct TextureDataStruct {
+      unsigned char Data[TextureHeaders[i].width * TextureHeaders[i].height];
+      unsigned char Palette[256 * 3];
+    };
+    TextureDataStruct textureData;
+  }
 };
